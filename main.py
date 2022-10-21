5ac1197440600b875568b3e1009167c5261dc8c6
Asad Jafri <51427124+Asadhjafri@users.noreply.github.com>
2022-10-21 17:37:33 +0000

    Add nested if else logic to verify age of rider. Costs associated with age input.

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
 if height >= 120:
-  print ("Can Ride rollercoaster!")
+    print("Can Ride rollercoaster!")
+    age = int(input("What is your age?"))
+    if age <= 18:
+        print("Please pay 7 dollars.")
+    else:
+        print("Please pay 12 dollars.")
 else:
-  print ("Can't ride rollercoaster")
+    print("Can't ride rollercoaster")
