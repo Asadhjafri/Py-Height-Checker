1b2a2dfd59fc42044e3e845c94ef00aaf3469db9
Asad Jafri <51427124+Asadhjafri@users.noreply.github.com>
2022-10-21 17:45:48 +0000

    add else if logic to check is rider is between 12 and 18. Added new fee for that range group.

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ height = int(input("What is your height in cm? "))
 if height >= 120:
     print("Can Ride rollercoaster!")
     age = int(input("What is your age?"))
-    if age <= 18:
+    if age < 12:
+      print("Please pay 5 dollars")
+    elif age <= 18:
         print("Please pay 7 dollars.")
     else:
         print("Please pay 12 dollars.")
