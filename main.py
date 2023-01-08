9539dfc37830ba492826f1f82179112030388ea8
Asad Jafri <51427124+Asadhjafri@users.noreply.github.com>
2023-01-08 19:21:31 +0000

    add logic to add amount to bill price is photo option is true. Add Yes or yes option to select photo and calculate price.

--- main.py
+++ main.py
@@ -3,18 +3,21 @@ height = int(input("What is your height in cm? "))
 bill = 0
 
 if height >= 120:
-    print("Can Ride rollercoaster!")
-    age = int(input("What is your age?"))
-    if age < 12:
-      bill = 5
-      print("Child tickets are 5 dollars")
-    elif age <= 18:
-      bill = 7
-      print("Youth tickets are 7 dollars.")
-    else:
-      bill = 12
-      print("Adult tickets are 12 dollars.")
-    wants_photo = input("Do you want a photo taken? Yes or No") 
-    if wants_photo == "Yes":
+  print("Can Ride rollercoaster!")
+  age = int(input("What is your age?"))
+  if age < 12:
+    bill = 5
+    print("Child tickets are 5 dollars")
+  elif age <= 18:
+    bill = 7
+    print("Youth tickets are 7 dollars.")
+  else:
+    bill = 12
+    print("Adult tickets are 12 dollars.")
+  wants_photo = input("Do you want a photo taken? Yes or No")
+  if wants_photo == "Yes" or "yes":
+    bill += 3
+
+  print(f"Your final bill amount is ${bill}")
 else:
-    print("Can't ride rollercoaster")
+  print("Can't ride rollercoaster")
