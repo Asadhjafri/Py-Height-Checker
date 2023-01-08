524c23a5b3bfd5bb8b83ccaacf883bc9ce84fa97
Asad Jafri <51427124+Asadhjafri@users.noreply.github.com>
2023-01-08 19:06:33 +0000

    Change print statements to pricing. Add Child, Youth and Adult price. Add variable asking if user wants a photo. Start if else template.

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height >= 120:
     print("Can Ride rollercoaster!")
     age = int(input("What is your age?"))
     if age < 12:
-      print("Please pay 5 dollars")
+      print("Child tickets are 5 dollars")
     elif age <= 18:
-        print("Please pay 7 dollars.")
+        print("Youth tickets are 7 dollars.")
     else:
-        print("Please pay 12 dollars.")
+        print("Adult tickets are 12 dollars.")
+    wants_photo = input("Do you want a photo taken? Yes or No") 
+    if wants_photo == "Yes":
 else:
     print("Can't ride rollercoaster")
