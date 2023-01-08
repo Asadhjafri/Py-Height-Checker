9a0cd7b15a66039aecb6b3a2e376888d2582cf18
Asad Jafri <51427124+Asadhjafri@users.noreply.github.com>
2023-01-08 19:09:25 +0000

    Add variable bill. Add bill price to each age group.

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ if height >= 120:
     print("Can Ride rollercoaster!")
     age = int(input("What is your age?"))
     if age < 12:
+      bill = 5
       print("Child tickets are 5 dollars")
     elif age <= 18:
-        print("Youth tickets are 7 dollars.")
+      bill = 7
+      print("Youth tickets are 7 dollars.")
     else:
-        print("Adult tickets are 12 dollars.")
+      bill = 12
+      print("Adult tickets are 12 dollars.")
     wants_photo = input("Do you want a photo taken? Yes or No") 
     if wants_photo == "Yes":
 else:
